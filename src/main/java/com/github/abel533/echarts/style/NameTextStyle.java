428fa2cdb1d8e7f71dc5f75890d2509c0a38b8ce
Alexandros Megalemos <a.megalemos@albourne.com>
2018-12-06 16:00:11 +0200

    : latest

--- src/main/java/com/github/abel533/echarts/style/NameTextStyle.java
+++ src/main/java/com/github/abel533/echarts/style/NameTextStyle.java
@@ -12,110 +12,120 @@ import java.io.Serializable;
  * @since 2016-02-28 17:21
  */
 public class NameTextStyle implements Serializable {
-    private static final long serialVersionUID = 1L;
-    /**
-     * 坐标轴名称文字的颜色
-     */
-    private String color;
-    /**
-     * 坐标轴名称文字字体的风格
-     */
-    private FontStyle fontStyle;
-    /**
-     * 坐标轴名称文字字体的粗细
-     */
-    private FontWeight fontWeight;
-    /**
-     * 坐标轴名称文字的字体系列
-     */
-    private String fontFamily;
-    /**
-     * 坐标轴名称文字的字体大小
-     */
-    private Double fontSize;
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 坐标轴名称文字的颜色
+	 */
+	private String color;
+	/**
+	 * 坐标轴名称文字字体的风格
+	 */
+	private FontStyle fontStyle;
+	/**
+	 * 坐标轴名称文字字体的粗细
+	 */
+	private FontWeight fontWeight;
+	/**
+	 * 坐标轴名称文字的字体系列
+	 */
+	private String fontFamily;
+	/**
+	 * 坐标轴名称文字的字体大小
+	 */
+	private Double fontSize;
 
-    public String color() {
-        return this.color;
-    }
+	private Double padding;
 
-    public NameTextStyle color(String color) {
-        this.color = color;
-        return this;
-    }
+	public String color() {
+		return this.color;
+	}
 
-    public FontStyle fontStyle() {
-        return this.fontStyle;
-    }
+	public NameTextStyle color(String color) {
+		this.color = color;
+		return this;
+	}
 
-    public NameTextStyle fontStyle(FontStyle fontStyle) {
-        this.fontStyle = fontStyle;
-        return this;
-    }
+	public FontStyle fontStyle() {
+		return this.fontStyle;
+	}
 
-    public FontWeight fontWeight() {
-        return this.fontWeight;
-    }
+	public NameTextStyle fontStyle(FontStyle fontStyle) {
+		this.fontStyle = fontStyle;
+		return this;
+	}
 
-    public NameTextStyle fontWeight(FontWeight fontWeight) {
-        this.fontWeight = fontWeight;
-        return this;
-    }
+	public FontWeight fontWeight() {
+		return this.fontWeight;
+	}
 
-    public String fontFamily() {
-        return this.fontFamily;
-    }
+	public NameTextStyle fontWeight(FontWeight fontWeight) {
+		this.fontWeight = fontWeight;
+		return this;
+	}
 
-    public NameTextStyle fontFamily(String fontFamily) {
-        this.fontFamily = fontFamily;
-        return this;
-    }
+	public String fontFamily() {
+		return this.fontFamily;
+	}
 
-    public Double fontSize() {
-        return this.fontSize;
-    }
+	public NameTextStyle fontFamily(String fontFamily) {
+		this.fontFamily = fontFamily;
+		return this;
+	}
 
-    public NameTextStyle fontSize(Double fontSize) {
-        this.fontSize = fontSize;
-        return this;
-    }
+	public Double fontSize() {
+		return this.fontSize;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public NameTextStyle fontSize(Double fontSize) {
+		this.fontSize = fontSize;
+		return this;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public FontStyle getFontStyle() {
-        return fontStyle;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public void setFontStyle(FontStyle fontStyle) {
-        this.fontStyle = fontStyle;
-    }
+	public FontStyle getFontStyle() {
+		return fontStyle;
+	}
 
-    public FontWeight getFontWeight() {
-        return fontWeight;
-    }
+	public void setFontStyle(FontStyle fontStyle) {
+		this.fontStyle = fontStyle;
+	}
 
-    public void setFontWeight(FontWeight fontWeight) {
-        this.fontWeight = fontWeight;
-    }
+	public FontWeight getFontWeight() {
+		return fontWeight;
+	}
 
-    public String getFontFamily() {
-        return fontFamily;
-    }
+	public void setFontWeight(FontWeight fontWeight) {
+		this.fontWeight = fontWeight;
+	}
 
-    public void setFontFamily(String fontFamily) {
-        this.fontFamily = fontFamily;
-    }
+	public String getFontFamily() {
+		return fontFamily;
+	}
 
-    public Double getFontSize() {
-        return fontSize;
-    }
+	public void setFontFamily(String fontFamily) {
+		this.fontFamily = fontFamily;
+	}
 
-    public void setFontSize(Double fontSize) {
-        this.fontSize = fontSize;
-    }
+	public Double getFontSize() {
+		return fontSize;
+	}
+
+	public void setFontSize(Double fontSize) {
+		this.fontSize = fontSize;
+	}
+
+	public Double getPadding() {
+		return padding;
+	}
+
+	public void setPadding(Double padding) {
+		this.padding = padding;
+	}
 }
